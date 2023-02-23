a029b17169246a452194b6ae218b4dd3858945d4
vipan kumar <vipan.kumar@mrm.com>
2023-02-23 06:12:07 +0530

    main-2

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,39 @@
 import React from "react";
 
 const Register = () => {
-  return <div>Register...</div>;
+  return (
+    <section className="flex justify-center items-center">
+      <form>
+        <div className="mb-6">
+          <input
+            type="text"
+            className="w-[21rem] rounded-lg bg-white py-1 px-3 text-[0.8125rem] leading-5 shadow-xl shadow-black/5 hover:bg-slate-50 ring-2 ring-indigo-200"
+            placeholder="Email"
+          />
+        </div>
+        <div className="mb-6">
+          <input
+            type="text"
+            className="w-[21rem] rounded-lg bg-white py-1 px-3 text-[0.8125rem] leading-5 shadow-xl shadow-black/5 hover:bg-slate-50 ring-2 ring-indigo-200"
+            placeholder="Password"
+          />
+        </div>
+        <div className="mb-6">
+          <input
+            type="text"
+            className="w-[21rem] rounded-lg bg-white py-1 px-3 text-[0.8125rem] leading-5 shadow-xl shadow-black/5 hover:bg-slate-50 ring-2 ring-indigo-200"
+            placeholder="Retype password"
+          />
+        </div>
+
+        <div className="input">
+          <button type="button" className="add-button">
+            submit
+          </button>
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default Register;
--- src/pages/Sign.jsx
+++ src/pages/Sign.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 const Sign = () => {
-  return <div>Sign in...</div>;
+  return (
+    <section className="flex justify-center items-center">
+      <form>
+        <div className="mb-6">
+          <input
+            type="text"
+            className="w-[21rem] rounded-lg bg-white py-1 px-3 text-[0.8125rem] leading-5 shadow-xl shadow-black/5 hover:bg-slate-50 ring-2 ring-indigo-200"
+            placeholder="User name"
+          />
+        </div>
+        <div className="mb-6">
+          <input
+            type="password"
+            className="w-[21rem] rounded-lg bg-white py-1 px-3 text-[0.8125rem] leading-5 shadow-xl shadow-black/5 hover:bg-slate-50 ring-2 ring-indigo-200"
+            placeholder="Password"
+          />
+        </div>
+        <div className="input">
+          <button type="button" className="add-button">
+            submit
+          </button>
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default Sign;
